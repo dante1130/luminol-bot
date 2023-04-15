1669f80de477bee712fb5075737583478bc21c24
dante1130 <andrewdante2@gmail.com>
2023-04-16 01:46:58 +0800

    Add chat command to chat with Ema

--- src/commands.rs
+++ src/commands.rs
@@ -13,7 +13,10 @@ use serenity::{
     prelude::{Context, TypeMapKey},
 };
 
-use async_openai::types::CreateCompletionRequestArgs;
+use async_openai::types::{
+    ChatCompletionRequestMessage, CreateChatCompletionRequestArgs, CreateCompletionRequestArgs,
+    Role,
+};
 
 use rand::seq::SliceRandom;
 
@@ -22,7 +25,7 @@ use rand::seq::SliceRandom;
 struct General;
 
 #[group]
-#[commands(complete)]
+#[commands(complete, chat)]
 struct OpenAI;
 
 pub fn framework() -> StandardFramework {
@@ -127,6 +130,58 @@ pub async fn complete(ctx: &Context, msg: &Message) -> CommandResult {
 
     msg.channel_id
         .say(&ctx.http, &response.choices.first().unwrap().text)
+        .await?;
+
+    Ok(())
+}
+
+#[command]
+pub async fn chat(ctx: &Context, msg: &Message) -> CommandResult {
+    let prompt = msg.content.trim_start_matches("e!chat").trim();
+
+    let request = CreateChatCompletionRequestArgs::default()
+        .model("gpt-3.5-turbo")
+        .messages(vec![
+            {
+                ChatCompletionRequestMessage {
+                    role: Role::System,
+                    content: "You are Ema Skye from the game Ace Attorney, you have an aspiration to be a forensic scientist. 
+                              You are cheerful and optimistic, especially when it comes to forensic science.".to_owned(),
+                    name: None,
+                }
+            },
+            {
+                ChatCompletionRequestMessage {
+                    role: Role::Assistant,
+                    content: "Ask away! With the power of science, 
+                              I'll scientifically analyze the data available
+                              and use my scientific gadgets to solve your problems.".to_owned(),
+                    name: Some("EmaSkye".to_owned()),
+                }
+            },
+            {
+                ChatCompletionRequestMessage {
+                    role: Role::User,
+                    content: prompt.to_owned(),
+                    name: Some(msg.author.name.to_owned()),
+                }
+            },
+        ])
+        .max_tokens(128_u16)
+        .build()
+        .unwrap();
+
+    let data = ctx.data.read().await;
+
+    let client = data.get::<OpenAIClient>().unwrap().get(&0).unwrap();
+
+    let response = client.chat().create(request).await.unwrap();
+
+    msg.channel_id
+        .say(
+            &ctx.http,
+            &response.choices.first().unwrap().message.content,
+        )
         .await?;
 
     Ok(())
