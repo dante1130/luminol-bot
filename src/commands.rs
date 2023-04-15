022b92d41fb83e6417285074dc64961679307aad
dante1130 <andrewdante2@gmail.com>
2023-04-15 23:20:51 +0800

    Have ask command return 8-ball responses

--- src/commands.rs
+++ src/commands.rs
@@ -13,6 +13,8 @@ use serenity::{
     prelude::Context,
 };
 
+use rand::seq::SliceRandom;
+
 #[group]
 #[commands(ping, ask)]
 struct General;
@@ -52,11 +54,36 @@ pub async fn ask(ctx: &Context, msg: &Message) -> CommandResult {
 
     let question = msg.content.trim_start_matches("e!ask").trim();
 
+    let responses = vec![
+        "as I see it, yes.",
+        "ask again later.",
+        "better not tell you now.",
+        "cannot predict now.",
+        "concentrate and ask again.",
+        "don't count on it.",
+        "it is certain.",
+        "it is decidedly so.",
+        "most likely.",
+        "my reply is no.",
+        "my sources say no.",
+        "outlook not so good.",
+        "outlook good.",
+        "reply hazy, try again.",
+        "signs point to yes.",
+        "very doubtful.",
+        "without a doubt.",
+        "yes.",
+        "yes - definitely.",
+        "you may rely on it.",
+    ];
+
+    let response = responses.choose(&mut rand::thread_rng()).unwrap();
+
     msg.channel_id
         .send_files(&ctx.http, vec!["res/DS.gif"], |m| {
             m.embed(|e| {
                 e.title(question);
-                e.description(prepend_response);
+                e.description(prepend_response.to_owned() + response);
                 e.color(0xF6DBD8);
                 e.attachment("DS.gif");
                 e
